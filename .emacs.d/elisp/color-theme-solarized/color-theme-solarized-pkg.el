3f6120f48aced1ca7661bd368b76dc1b399f2a9e
Matsui Wataru <wataru.matsui@nubee.com>
2013-10-21 20:41:20 +0900

    Removed reference library files

--- .emacs.d/elisp/color-theme-solarized/color-theme-solarized-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "color-theme-solarized" "20121013" "Solarized themes for Emacs" '((color-theme "6.5.5")))
--- .emacs.d/elpa/flex-autopair-0.3/flex-autopair-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flex-autopair" "0.3" "Automatically insert pair braces and quotes, insertion conditions & actions are highly customizable." (quote nil))
