5a333573c3e85bf99773399afdc53675effb85a2
wataru.matsui <wataru.matsui@nubee.com>
2014-04-25 10:15:03 +0900

    Modified emacs setting. Modified bash aliases. Added iTerm2 setting.

--- .emacs.d/conf/02_powerline.el
+++ .emacs.d/conf/02_powerline.el
@@ -1,9 +1,9 @@
 ;; color-themeより前だとカラーリングがおかしくなる
 
 ;; vim-powerlineの様なインターフェイス
-(when (require 'powerline nil t)
-  (custom-set-faces
-   '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
-   '(mode-line-buffer-id ((t (:background "gray75" :foreground "dark cyan" :weight bold :height 1.0))))
-   '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
-  (powerline-default-theme))
+;; (when (require 'powerline nil t)
+;;   (custom-set-faces
+;;    '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+;;    '(mode-line-buffer-id ((t (:background "gray75" :foreground "dark cyan" :weight bold :height 1.0))))
+;;    '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+;;   (powerline-default-theme))
