f1c00ff273c212b1f6fd05635f678fa0093e6e61
watawuwu <watawuwu@gmail.com>
2015-05-07 22:06:03 +0900

    Added the Haskell settings

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -497,6 +497,24 @@ redrawが non-nilの場合は、Windowを再描画します。"
 (when (require 'coffee-mode nil t)
   (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode)))
 
+;; Haskell-mode
+(when (require 'haskell-mode nil t)
+  ;; indent の有効.
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'font-lock-mode)
+  (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+  (add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
+  (add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+  (setq haskell-program-name "/usr/local/bin/ghci")
+  (defadvice inferior-haskell-load-file (after change-focus-after-load)
+    "Change focus to GHCi window after C-c C-l command"
+    (other-window 1))
+  (ad-activate 'inferior-haskell-load-file)
+
+  (add-hook 'haskell-mode-hook (lambda () (flymake-mode)))
+  )
 
 (defun gker-setup-sh-mode ()
   "My own personal preferences for `sh-mode'.
