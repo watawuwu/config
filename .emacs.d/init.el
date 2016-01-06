53f938c3e0715866702f71ced2181fc90f07f488
watawuwu <watawuwu@gmail.com>
2016-01-06 10:58:54 +0900

    Added the shell environment for zsh

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,6 +24,19 @@
         (and (stringp docstring)
              (get-text-property 0 'dynamic-docstring-function docstring)))))
 
+
+(defun exists-shell ()
+  (or (executable-find "zsh")
+      (executable-find "bash")
+      (executable-find "cmdproxy")
+      (error "can't find 'shell' command in PATH!!")))
+
+;; set shell environment
+(setq shell-file-name (exists-shell))
+(setenv "SHELL" shell-file-name)
+(setq explicit-shell-file-name shell-file-name)
+
+
 (defun set-exec-path-from-shell-PATH ()
   "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
    This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
@@ -52,6 +65,7 @@
 
 ;; add load-path directory
 (add-to-load-path "el-get")
+(add-to-load-path "private")
 
 
 
