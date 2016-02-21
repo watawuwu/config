a3b502da2b9f61f2ccb10415aa66ec0b072572c0
watawuwu <watawuwu@gmail.com>
2016-02-21 18:01:41 +0900

    Modified the indent spaces num of coffee-mode

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -485,6 +485,13 @@ redrawが non-nilの場合は、Windowを再描画します。"
 
 ;; coffeeモード
 (when (autoload-if-found 'coffee-mode "coffee-mode")
+  (defun coffee-custom ()
+    "coffee-mode-hook"
+    (set (make-local-variable 'tab-width) 2)
+    (setq coffee-tab-width 2))
+
+  (add-hook 'coffee-mode-hook
+            '(lambda() (coffee-custom)))
   (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode)))
 
 ;; tomlモード
