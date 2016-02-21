2bb1bd0ed001f575e1e9b08c5d58978273ee7071
watawuwu <watawuwu@gmail.com>
2016-02-21 18:18:46 +0900

    Fixed the syntax error of rust config

--- .emacs.d/conf/50_rust.el
+++ .emacs.d/conf/50_rust.el
@@ -7,7 +7,7 @@
    '(racer-cmd (expand-file-name "~/.multirust/toolchains/stable/cargo/bin/racer"))
    ;; https://github.com/brson/multirust/issues/77
    '(racer-rust-src-path (expand-file-name "~/.multirust/toolchains/1.6.0/src")))
-   '(rust-indent-offset 4))
+   '(rust-indent-offset 4)
 
   ;; Load rust-mode when you open `.rs` files
   (add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
